0324926fc0675f1024828fc5732f415100d9816d
Saleik <sarrazinkevin@free.fr>
2021-02-16 15:30:53 +0100

    Update introduction.jsx content

--- src/App/Introduction.jsx
+++ src/App/Introduction.jsx
@@ -26,7 +26,7 @@ export const Introduction = () => {
                     <div className="text-center col-md-10 offset-md-1">
                         <Title fontSize="1.3rem" fontWeight="400" color="white">Bienvenue ! Moi c'est Kevin, heureux de vous rencontrer.</Title>
                         <p>
-                            Après une première expérience de manager, le besoin de revenir à mes premières passions était trop fort. De nature curieux, j'aime partir à la découverte de nouveau framework, essayer de nouvelle features, avec l'objectif d'éprouver continuellement mon code. Mon credo, c'est d'offrir un service technique moderne et une relation client de qualité.
+                            Après une première expérience de manager, le besoin de revenir à ma première passion, l'informatique, était trop fort. De nature curieux, j'aime partir à la découverte de nouveau framework, essayer de nouvelle features, avec l'objectif d'éprouver continuellement mon code. Mon credo, c'est d'offrir un service technique moderne et une relation client de qualité.
                  </p>
                     </div>
                 </div>
